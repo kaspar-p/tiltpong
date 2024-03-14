0d105f3e26fc3a9f609c78af14257870c8c5959e
hisbaan <hisbaan@gmail.com>
2024-03-14 08:03:21 -0400

    client: implement event queue

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -1,11 +1,34 @@
 #include "mbed.h"
+#include "coap.h"
 #include "sensors.h"
+
+EventQueue queue;
+InterruptIn button1(BUTTON1);
+
+bool button_pressed = false;
+
+void get_sensor_data() {
+  if (button_pressed) {
+    read_accelerometer();
+    read_gyro();
+    button_pressed = false;
+  }
+}
+
+void button_fall_handler() {
+  if (button1.read() == 0) {
+    button_pressed = true;
+  }
+}
 
 int main() {
   accelerometer_init();
   gyro_init();
-  while (1) {
-    read_accelerometer();
-    read_gyro();
-  }
+
+  queue.call_every(5ms, button_fall_handler);
+  queue.call_every(200ms, get_sensor_data);
+  queue.dispatch_forever();
+
+  // should never reach here
+  return 0;
 }
